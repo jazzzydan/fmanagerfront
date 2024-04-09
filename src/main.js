41c6cc5c5ea482099a4f9141ce9ef0f315b6a159
kevinp1234 <kevin_paavo@hotmail.com>
2024-04-09 13:41:58 +0300

    master updated settings

--- src/main.js
+++ src/main.js
@@ -1,5 +1,27 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueAxios from "vue-axios";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.js"
 
-createApp(App).use(router).mount('#app')
+
+// Font Awesome imports
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret, fas, faR } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+
+// Add the imported icons to the library
+library.add(faUserSecret, fas, faR, far)
+
+const app = createApp(App)
+
+app.use(router)
+app.use(VueAxios, axios)
+
+// Register the FontAwesomeIcon component globally
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.mount('#app')
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <font-awesome-icon :icon="['fas', 'user-secret']" />
   </div>
 </template>
