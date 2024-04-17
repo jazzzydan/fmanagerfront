f8d8d7c76b1ed8afabec75b1135732c5f0b89903
uvemae <uve.mae@gmail.com>
2024-04-17 09:55:43 +0300

    +dropdowns

---
+++ src/components/dropdown/LeagueDropdown.vue
@@ -0,0 +1,53 @@
+<template>
+  <select v-model="selectedLeagueId"
+          @change="emitSelectedLeagueId"
+          ref="emitSelectedClubId"
+          class="form-select">
+    <option selected value="0">All leagues</option>
+    <option v-for="league in leagues" :value="league.leagueId" :key="league.leagueId">
+      {{league.leagueName}}</option>
+  </select>
+</template>
+
+<script>
+import router from "@/router";
+
+export default {
+  name: "LeagueDropdown",
+  data() {
+    return {
+      selectedCountryId: 0,
+      selectedLeagueId: 0,
+      leagues: [
+        {
+          leagueId: 0,
+          leagueName: '',
+        }
+      ]
+    }
+  },
+  methods: {
+    sendGetLeaguesRequest() {
+      this.$http.get(`/leagues/${this.selectedCountryId}`)
+          .then(response => {
+            this.leagues = response.data
+          })
+          .catch(error => {
+            router.push({name: 'errorRoute'})
+          })
+    },
+
+    getSelectedClubId(countryId) {
+      this.selectedCountryId = countryId
+      this.sendGetLeaguesRequest()
+    },
+
+    emitSelectedLeagueId() {
+      this.$emit('event-selected-league-change', this.selectedLeagueId)    // 'event-selected-league-change' ... !!!
+    }
+  },
+  beforeMount() {
+    this.sendGetLeaguesRequest()
+  }
+}
+</script>
